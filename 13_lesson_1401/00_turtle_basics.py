4b6365bf04a1d21752c03fb904dac23996327397
demidraksin <greatraksin@icloud.com>
2022-01-14 10:21:21 +0300

    Adding turtle lesson

---
+++ 13_lesson_1401/00_turtle_basics.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+t = Turtle()
+t.shape('turtle')
+
+# все команды пишем здесь
+t.forward(100)  # идти вперед на 100 точек
+t.left(45)  # поворот влево на 45 градусов
+t.forward(150)
+
+done()
+
+
+
