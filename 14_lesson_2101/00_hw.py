0df5b2149411ca9c0d628ac8bfcfc3f6ccfaf766
demidraksin <greatraksin@icloud.com>
2022-01-21 09:22:26 +0300

    Filling house

--- 14_lesson_2101/00_hw.py
+++ 14_lesson_2101/00_hw.py
@@ -27,10 +27,11 @@ t.down()
 t.color('#8c271d')
 t.fillcolor('#c9392c')
 
-
+t.begin_fill()
 for i in range(3):
     t.fd(140)
     t.lt(120)
+t.end_fill()
 
 x = -40
 y = -45
@@ -38,20 +39,32 @@ t.up()
 t.goto(x, y)
 t.down()
 
+t.color('#3d2318')
+t.fillcolor('#693d2b')
+
+t.begin_fill()
 for i in range(2):
     t.fd(30)
     t.rt(90)
     t.fd(55)
     t.rt(90)
+t.end_fill()
 
 x = 10
 t.up()
 t.goto(x, y)
 t.down()
 
+t.color('#96491a')  # цвет линии
+t.fillcolor('#3c83b0')  # цвет заливки
+
+t.begin_fill()
 for i in range(4):
     t.fd(30)
     t.rt(90)
+t.end_fill()
 
+t.up()
+t.goto(400, 400)
 
 done()
